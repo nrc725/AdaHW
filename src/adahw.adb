9453a4d5ae1763798927efcdcf40084c3e744026
Greg Fletcher <gnfletcher@gmail.com>
2017-10-12 11:49:52 -0400

    Input loops.

--- src/adahw.adb
+++ src/adahw.adb
@@ -8,16 +8,31 @@
 with Ada.Text_IO, Ada.Integer_Text_IO;
 use  Ada.Text_IO, Ada.Integer_Text_IO;
 procedure Adahw is
-   --Declare array for patient data input, can replace if I can total
-        --in real time.
+   --Declare an integer for the number of records. First input.
+   n, id, sick, test1, test2 : Integer;
    --Declare array to hold the test totals.
+   totals: array (1..4) of Integer;
    --Declare array for output.
+   probs: array (1..4) of Float;
 begin
    --For loop to get patient data input and insert into an array.
-   Get();
-   --For loop to read patient data array and sum the test totals.
-
+   Get(n);
+   for N in n loop
+      Get(id);
+      Get(sick);
+      Get(test1);
+      Get(test2);
+      if sick = 1 then
+         --Increase Positive test totals
+         if test1
+      else
+         --Increase Negative test totals
+      end loop;
+   end loop;
    --Function to calculate and output probabilities.
+
+
+   --Outputs
    Put();
-   null;
+   return;
 end Adahw;
