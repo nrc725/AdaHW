55659c02e3b30f9327d25cfd1adfb507abfc4079
Greg Fletcher <gnfletcher@gmail.com>
2017-10-12 11:07:05 -0400

    Commented program outline.

--- src/adahw.adb
+++ src/adahw.adb
@@ -1,6 +1,23 @@
+-------------------------------------------------------------------------
+--
+--  Program: Ada Homework
+--  Purpose: Take in test result data and output conditional probability
+--  Author: Greg Fletcher, Jason Fazio, Nick Currie
+--
+-------------------------------------------------------------------------
+with Ada.Text_IO, Ada.Integer_Text_IO;
+use  Ada.Text_IO, Ada.Integer_Text_IO;
 procedure Adahw is
-
+   --Declare array for patient data input, can replace if I can total
+        --in real time.
+   --Declare array to hold the test totals.
+   --Declare array for output.
 begin
-   --  Insert code here.
+   --For loop to get patient data input and insert into an array.
+   Get();
+   --For loop to read patient data array and sum the test totals.
+
+   --Function to calculate and output probabilities.
+   Put();
    null;
 end Adahw;
